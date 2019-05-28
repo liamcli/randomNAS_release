66f34e7445671086bcbb297a8b33dab046090de4
mula0513 <mula0513@gmail.com>
2019-05-27 23:08:02 -0400

    change args for run_experiment.sh

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -7,4 +7,4 @@ RESULTS_DIR=/results
 cd /opt/randomNAS/searchers
 git checkout origin/development 
 export PYTHONPATH=/opt/darts_fork:/opt/randomNAS:/opt/darts_fork/cnn
-python -u random_weight_share.py --data_dir $DATA_DIR --benchmark cnn --epochs 150 --batch_size 64 --grad_clip 1 --init_channels 24 --seed $1 --save_dir $RESULT_DIR/cnn/random/trial$1 --save_to_remote
+python -u random_weight_share.py --data_dir $DATA_DIR --benchmark cnn --seed $1 --epochs $2 --batch_size 64 --grad_clip 1 --init_channels $3 --save_dir $RESULT_DIR/cnn/random/trial$1 --save_to_remote
